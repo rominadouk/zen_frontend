9f11481c9116a4193076b2a37af672ce39c9fc6d
rominadouk <douk.romina@gmail.com>
2023-04-17 00:54:39 -0700

    removed react strictmode for production

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import 'bootstrap/dist/js/bootstrap.bundle'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  //<React.StrictMode>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-  </React.StrictMode>
+  //</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
